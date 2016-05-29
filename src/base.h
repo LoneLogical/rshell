2a016d31dac336f2829c5ae2c4c5a9a78e65247f
Destin Raymundo <Destin@MacBook-Pro.home>
2016-05-28 22:34:48 -0700

    deleted some commented stuff in base.h

--- src/base.h
+++ src/base.h
@@ -25,24 +25,6 @@ class Exit : public Command {
 
 class Test : public Command {
 	private:
-		/*
-		struct stat{
-			dev_t     st_dev;     
-			ino_t     st_ino;     
-			mode_t    st_mode;    
-			nlink_t   st_nlink;   
-			uid_t     st_uid;     
-			gid_t     st_gid;     
-		    dev_t     st_rdev;    
-			off_t     st_size;    
-			blksize_t st_blksize; 
-			blkcnt_t  st_blocks;  
-			time_t    st_atime; 
-			time_t    st_mtime; 
-			time_t    st_ctime;
-		};
-		*/
-	public:
 		Test(char** arr);
 		virtual bool execute();
 };
