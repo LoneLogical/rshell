e5c15e0bb2095226edcc65c87fd9027a4ff17d21
Andrew Whitaker <awhit023@hammer.cs.ucr.edu>
2016-05-30 10:13:20 -0700

    fixed recursion

--- src/main.cpp
+++ src/main.cpp
@@ -136,12 +136,9 @@ Base* build_tree(vector<char> connects, unsigned int &con_index, char* &cmdstr) 
              
             child = build_tree(connects, con_index, cmdstr);
             
-            if (prev == NULL) {
-                prev = child;
-            }
-            else {    
+            if (prev != NULL) {
                 prev->set_rhs(child);
-            }
+            } // here is where we might mess up
 
             ++con_index;
             cout << "con_index = " << con_index << endl;
@@ -166,18 +163,23 @@ Base* build_tree(vector<char> connects, unsigned int &con_index, char* &cmdstr) 
                 }
                 cout << "tokenAlpha = " << token << endl;
                 cout << "cmdstr = " << cmdstr << endl;
-                //token = NULL;
 
-                //if (d == ')') {
-                //token = NULL;
 
                 if (d == ')') {
-                    cout << "does it return from here???" << endl;
-                    return prev;
+                    if (prev == NULL) {
+                        cout << "immediately return child" << endl;
+                        return child;
+                    }
+                    else {
+                        cout << "does it return from here???" << endl;
+                        //prev = child;
+                        return prev;
+                    }
                 }
                 cout << "limbo 2" << endl;
             }
             else {
+                prev = child;
                 cout << "limbo 1" << endl;
                 return prev;
             }
@@ -191,15 +193,7 @@ Base* build_tree(vector<char> connects, unsigned int &con_index, char* &cmdstr) 
             child = make_command(token);
             cout << "command made" << endl;
         }
-            // if else statements to make a connector!
 
-            //if ( (connects.size() > 0) && (con_index == connects.size()) ) {
-                //prev->set_rhs(command);
-                //user = prev;
-                //con_index = con_index + 1;
-            //}
-            //else if (
-            //else {
         string deltemp(del);
         if (deltemp == ";") {
             curr = new Semicolon();
@@ -240,8 +234,15 @@ Base* build_tree(vector<char> connects, unsigned int &con_index, char* &cmdstr) 
             curr->set_lhs(child);
         }
         else {
-            prev->set_rhs(child);
-            curr->set_lhs(prev);
+            if (prev->check_type()) {
+                cout << "prev was a connector" << endl;
+                prev->set_rhs(child);
+                curr->set_lhs(prev);
+            }
+            else {
+                cout << "prev was a command" << endl;
+                curr->set_lhs(prev);
+            }
         }
         prev = curr;
                 
@@ -264,7 +265,7 @@ Base* build_tree(vector<char> connects, unsigned int &con_index, char* &cmdstr) 
 
         
     } while (con_index < connects.size());
-
+    cout << "broke the while loop" << endl;
     return prev;
 }
 
@@ -342,146 +343,5 @@ Base* make_command(char* token) {
 
 
 
-
-
-
-/* this is just the scrapyard */
-
-
-
-
-/*
-
-    if (conns.size() == 0) {
-        small = strtok(str2, " ");
-        while (small != NULL)
-        {
-            //cout << '_' << small << '_'; 
-            args.push_back(small);
-            small = strtok(NULL, " ");
-        }
-        //cout << endl;
-
-        cmdarr = new char*[args.size()+ 1];
-
-        for (unsigned int a = 0; a < args.size(); ++a) {
-            string temp(args.at(a)); // turn it into string so i can use size()
-            cmdarr[a] = new char[temp.size()]; //allocate memory for each char arr[]
-            strcpy(cmdarr[a], args.at(a)); //copy c_str from vector into array
-        }
-
-        cmdarr[args.size()] = NULL;
-        string check_exit(cmdarr[0]);
-        if (check_exit == "exit") {
-            command = new Exit(cmdarr);
-        }
-        else {
-            command  = new Command(cmdarr);
-        }
-        user = command;
-        user->execute();
-        return;   
-    }
-
-    else {
-        unsigned int j = 0;
-        d = conns.at(j);
-        del[0] = d;
-        del[1] = '\0';
-        // takes in the first delimiter in command line
-        // used to create tokens of commands + flags
-
-        token = strtok_r(str2, del, &saved);
-        while ( (token != NULL) && (j <= conns.size()) ) {
-            //cout << "Token: " << token << endl;
-            small = strtok(token, " ");
-            while (small != NULL)
-            {
-                //cout << '_' << small << '_'; 
-                args.push_back(small);
-                small = strtok(NULL, " ");
-            }
-            //cout << endl;
-            // for loop to convert vector<char*> into char* array[]
-            // then create a command object by passing in array
-
-            cmdarr = new char*[args.size() + 1];
-
-            for (unsigned int a = 0; a < args.size(); ++a) {
-                string temp(args.at(a)); // turn it into string so i can use size()
-                cmdarr[a] = new char[temp.size()]; //allocate memory for each char arr[]
-                strcpy(cmdarr[a], args.at(a)); //copy c_str from vector into array
-            }
-
-            cmdarr[args.size()] = NULL; //puts null into last element of array
-
-
-            string chk_exit(cmdarr[0]);
-
-            // if statement to test if array[0] == exit
-            if(chk_exit == "exit") {
-                command = new Exit(cmdarr);
-            }
-            else {
-                command = new Command(cmdarr);
-            }
-
-            // if else statements to make a connector!
-
-            if (j == conns.size()) { //changed from token == NULL
-                prev->set_rhs(command);
-                user = prev;
-                j = j + 1;
-            }
-            else {
-                string deltemp(del);
-                if (deltemp == ";") {
-                    curr = new Semicolon();
-                    //cout << "made semi" << endl;
-                }
-                else if (deltemp == "&") {
-                    curr = new Ampersand();
-                    //cout << "made amper" << endl;
-                    saved = saved + 1;
-                }
-                else if (deltemp == "|") {
-                    curr = new Verticalbars();
-                // places command and connector objects in correct place
-                if (prev == NULL) {
-                    curr->set_lhs(command);
-                }
-                else {
-                    prev->set_rhs(command);
-                    curr->set_lhs(prev);
-                }
-                prev = curr;
-                
-
-                args.resize(0);
-                j = j + 1;
-                if (j < conns.size()) {
-
-                    d = conns.at(j);
-                    del[0] = d;
-                    del[1] = '\0';
-                    //deltemp = del;
-                    //cout << "del" << deltemp << endl;
-                    token = strtok_r(saved, del, &saved);
-                }
-                else {
-                    token = saved;
-                }
-            }
-            
-
-        }
-        //cout << "out here" << endl;
-        user = prev;
-        user->execute();
-    } 
-    return; 
-}   
-
-*/
 
 
